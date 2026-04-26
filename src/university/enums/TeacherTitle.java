39f5ffcc884fd02abe0dccc12949290e4ebd5efa
Olzhas <o_kuzembayev@kbtu.kz>
2026-04-27 01:28:08 +0500

    techertitle

--- src/university/enums/TeacherTitle.java
+++ src/university/enums/TeacherTitle.java
@@ -1,13 +1,5 @@
 package university.enums;
 
 public enum TeacherTitle {
-<<<<<<< HEAD
-    Tutor, Lecturer, SeniorLecturer, Professor; 
+    Tutor, Lecturer, SeniorLecturer, Professor;
 }
-=======
-    TUTOR,
-    LECTURER,
-    SENIOR_LECTURER,
-    PROFESSOR
-}
->>>>>>> 889aaa7 (Add academic module)
